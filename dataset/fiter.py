b0b53aadd2ed33427520a24c2d6352b946dc5743
lucaSartore <lucasar.com8@gmail.com>
2023-12-17 17:30:46 +0100

    add filter template

---
+++ dataset/fiter.py
@@ -0,0 +1,15 @@
+# I want to remove some information from the original data that could point the neural network to the specific year...
+# To avoid overfitting
+# To do so i will remove the year as an information, and keep only the month and day
+# also the value of the index could int to the year, so instead of the absolute value i will keep the 
+# gain or loss over the previous day
+import pandas as pd
+import numpy as np
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
